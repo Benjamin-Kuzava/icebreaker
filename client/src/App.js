4c16c7990f5e32c5a2b57b0a8bab19bf40c86944
benjamin kuzava <benjamin@kuzava.local>
2021-01-20 11:43:33 -0500

    Added basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { baseURL, config } from './services';
 
 function App() {
+  const [levels, setLevels] = useState([])
 
   useEffect(() => {
     const getLevels = async () => {
       const resp = await axios.get(baseURL, config);
       console.log(resp.data.records);
+      setLevels(resp.data.records);
     }
     getLevels();
-  }, [])
+  }, []);
 
   return (
     <div className="App">
-      <h1>test</h1>
+      <Route exact path='/'>
+        <h1>This is the Home Page</h1>
+      </Route>
+      <Route path='/new'>
+        <h2>This is the Level Create Page</h2>
+      </Route>
+      <Route path='/select'>
+        <h2>This is the Level Select Page</h2>
+      </Route>
     </div>
   );
 }
